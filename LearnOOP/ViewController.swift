2049d85900f2acc71e8a8da3af0054bf7b7cacfc
Beny Untoro <beny.untoro@gmail.com>
2019-05-15 12:17:17 +0700

    Initial Commit

---
+++ LearnOOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnOOP
+//
+//  Created by Beny Untoro on 15/05/19.
+//  Copyright © 2019 Beny Untoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
